0bec00ab251d5bbaa45ae52dfbebd862054ccf25
ntwalii <ntwaliaubin@gmail.com>
2024-10-11 17:43:30 +0200

    basic crud operations

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { NinjasService } from './ninjas.service';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Controller('ninjas')
+export class NinjasController {
+  constructor(private readonly ninjasService: NinjasService) {}
+
+  @Post()
+  create(@Body() createNinjaDto: CreateNinjaDto) {
+    return this.ninjasService.create(createNinjaDto);
+  }
+
+  @Get()
+  findAll(@Query('name') name: string) {
+    return this.ninjasService.findAll(name);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.ninjasService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return this.ninjasService.update(+id, updateNinjaDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.ninjasService.remove(+id);
+  }
+}
---
+++ src/ninjas/ninjas.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+
+@Injectable()
+export class NinjasService {
+  private ninjas = [
+    { id: 1, name: 'Naruto', rank: 'Genin' },
+    { id: 2, name: 'Sasuke', rank: 'Genin' },
+    { id: 3, name: 'Sakura', rank: 'Genin' },
+  ];
+  create(createNinjaDto: CreateNinjaDto) {
+    const ninja = {
+      id: this.ninjas.length + 1,
+      ...createNinjaDto,
+    };
+    this.ninjas.push(ninja);
+    return createNinjaDto;
+  }
+
+  findAll(name?: string) {
+    if (name) {
+      return this.ninjas.filter((ninja) => ninja.name === name);
+    }
+    return this.ninjas;
+  }
+
+  findOne(id: number) {
+    const ninja = this.ninjas.find((ninja) => ninja.id === id);
+    return ninja;
+  }
+
+  update(id: number, updateNinjaDto: UpdateNinjaDto) {
+    this.ninjas = this.ninjas.map((ninja) => {
+      if (ninja.id === id) {
+        return { ...ninja, ...updateNinjaDto };
+      }
+      return ninja;
+    });
+    return updateNinjaDto;
+  }
+
+  remove(id: number) {
+    this.ninjas = this.ninjas.filter((ninja) => ninja.id !== id);
+    return `Ninja with id ${id} has been removed`;
+  }
+}
